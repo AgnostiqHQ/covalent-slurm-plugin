5249af488bf477db225fd20472ba800ccb8a8e10
Sankalp Sanand <sankalp@agnostiq.ai>
2022-08-22 23:05:06 -0400

    since there were many ssh commands, using asyncssh now

--- covalent_slurm_plugin/slurm.py
+++ covalent_slurm_plugin/slurm.py
@@ -24,9 +24,8 @@ import asyncio
 import os
 import re
 import sys
-import tempfile
 from copy import deepcopy
-from typing import Any, Callable, Dict, List, Union
+from typing import Any, Callable, Dict, List, Union, Tuple
 
 import aiofiles
 import cloudpickle as pickle
@@ -35,6 +34,9 @@ from covalent._results_manager.result import Result
 from covalent._shared_files import logger
 from pathlib import Path
 from covalent.executor.base import BaseAsyncExecutor
+from covalent._shared_files.config import get_config
+import asyncssh
+import socket
 
 app_log = logger.app_log
 log_stack_info = logger.log_stack_info
@@ -43,9 +45,9 @@ _EXECUTOR_PLUGIN_DEFAULTS = {
     "username": "",
     "address": "",
     "ssh_key_file": "",
-    "remote_workdir": "",
+    "remote_workdir": "workdir",
     "poll_freq": 30,
-    "cache_dir": str(Path("~/.cache/covalent").expanduser().resolve()),
+    "cache_dir": str(Path(get_config("dispatcher.cache_dir")).expanduser().resolve()),
     "options": {
         "parsable": "",
     },
@@ -73,39 +75,80 @@ class SlurmExecutor(BaseAsyncExecutor):
         address: str,
         ssh_key_file: str,
         remote_workdir: str,
-        poll_freq: int,
         options: Dict,
+        poll_freq: int = 30,
         cache_dir: str = str(Path("~/.cache/covalent").expanduser().resolve()),
+        do_cleanup: bool = False,
         **kwargs,
     ):
         super().__init__(**kwargs)
 
         self.username = username
         self.address = address
-        self.ssh_key_file = ssh_key_file
+
+        ssh_key_file = ssh_key_file or get_config("executors.ssh.ssh_key_file")
+        self.ssh_key_file = str(Path(ssh_key_file).expanduser().resolve())
+
         self.remote_workdir = remote_workdir
         self.poll_freq = poll_freq
-        self.cache_dir = cache_dir
+
+        self.cache_dir = cache_dir or get_config("dispatcher.cache_dir")
+        self.cache_dir = str(Path(self.cache_dir).expanduser().resolve())
+
         self.options = deepcopy(options)
 
-    async def run_async_subprocess(self, cmd: List[str]):
+        self.do_cleanup = do_cleanup
 
-        command = " ".join(cmd)
-        app_log.debug(f"Command running: {command}")
+    # async def run_async_subprocess(self, cmd: List[str]):
 
-        proc = await asyncio.create_subprocess_shell(
-            command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
-        )
+    #     command = " ".join(cmd)
+    #     app_log.debug(f"Command running: {command}")
 
-        stdout, stderr = await asyncio.wait_for(proc.communicate(), timeout=10)
+    #     proc = await asyncio.create_subprocess_shell(
+    #         command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    #     )
 
-        print(f"[{command!r} exited with {proc.returncode}]")
-        if stdout:
-            print(f"\n{stdout.decode()}")
-        if stderr:
-            print(f"\n{stderr.decode()}", file=sys.stderr)
+    #     stdout, stderr = await asyncio.wait_for(proc.communicate(), timeout=10)
 
-        return proc.returncode, stdout, stderr
+    #     print(f"[{command!r} exited with {proc.returncode}]")
+    #     if stdout:
+    #         print(f"\n{stdout.decode()}")
+    #     if stderr:
+    #         print(f"\n{stderr.decode()}", file=sys.stderr)
+
+    #     return proc.returncode, stdout, stderr
+
+    async def _client_connect(self) -> Tuple[bool, asyncssh.SSHClientConnection]:
+        """
+        Helper function for connecting to the remote host through the paramiko module.
+
+        Args:
+            None
+
+        Returns:
+            True if connection to the remote host was successful, False otherwise.
+        """
+
+        ssh_success = False
+        conn = None
+        if os.path.exists(self.ssh_key_file):
+            try:
+                conn = await asyncssh.connect(
+                    self.address,
+                    username=self.username,
+                    client_keys=[self.ssh_key_file],
+                    known_hosts=None,
+                )
+
+                ssh_success = True
+            except (socket.gaierror, ValueError, TimeoutError) as e:
+                app_log.error(e)
+
+        else:
+            message = f"no SSH key file found at {self.ssh_key_file}. Cannot connect to host."
+            app_log.error(message)
+
+        return ssh_success, conn
 
     def _format_submit_script(
         self,
@@ -188,7 +231,7 @@ wait
 
         return slurm_preamble + slurm_conda + slurm_python_version + slurm_body
 
-    async def get_status(self, info_dict: dict) -> Union[Result, str]:
+    async def get_status(self, info_dict: dict, conn: asyncssh.SSHClientConnection) -> Union[Result, str]:
         """Query the status of a job previously submitted to Slurm.
 
         Args:
@@ -203,28 +246,11 @@ wait
         job_id = info_dict.get("job_id")
         if job_id is None:
             return Result.NEW_OBJ
+        
+        proc = await conn.run(f"scontrol show job {job_id}")
+        return proc.stdout.strip()
 
-        _, proc_stdout, _ = await self.run_async_subprocess(
-            [
-                "ssh",
-                "-i",
-                self.ssh_key_file,
-                "-o",
-                "StrictHostKeyChecking=no",
-                "-o",
-                "UserKnownHostsFile=/dev/null",
-                "-o",
-                "LogLevel=ERROR",
-                f"{self.username}@{self.address}",
-                "/bin/bash",
-                "-l",
-                "-c",
-                f"'scontrol show job {job_id}'",
-            ],
-        )
-        return proc_stdout.decode("utf-8").strip()
-
-    async def _poll_slurm(self, job_id: int) -> None:
+    async def _poll_slurm(self, job_id: int, conn: asyncssh.SSHClientConnection) -> None:
         """Poll a Slurm job until completion.
 
         Args:
@@ -235,7 +261,7 @@ wait
         """
 
         # Poll status every `poll_freq` seconds
-        status = await self.get_status({"job_id": str(job_id)})
+        status = await self.get_status({"job_id": str(job_id)}, conn)
 
         while (
             "PENDING" in status
@@ -244,12 +270,12 @@ wait
             or "CONFIGURING" in status
         ):
             await asyncio.sleep(self.poll_freq)
-            status = await self.get_status({"job_id": str(job_id)})
+            status = await self.get_status({"job_id": str(job_id)}, conn)
 
         if "COMPLETED" not in status:
             raise RuntimeError("Job failed with status:\n", status)
 
-    async def _query_result(self, result_filename: str, task_results_dir: str) -> Any:
+    async def _query_result(self, result_filename: str, task_results_dir: str, conn: asyncssh.SSHClientConnection) -> Any:
         """Query and retrieve the task result including stdout and stderr logs.
 
         Args:
@@ -259,82 +285,44 @@ wait
         Returns:
             result: Task result.
         """
+
         # Check the result file exists on the remote backend
         remote_result_filename = os.path.join(self.remote_workdir, result_filename)
-        return_code, _, proc_stderr = await self.run_async_subprocess(
-            [
-                "ssh",
-                "-i",
-                self.ssh_key_file,
-                "-o",
-                "StrictHostKeyChecking=no",
-                "-o",
-                "UserKnownHostsFile=/dev/null",
-                "-o",
-                "LogLevel=ERROR",
-                f"{self.username}@{self.address}",
-                "test",
-                "-e",
-                remote_result_filename,
-            ],
-        )
-        if return_code != 0:
-            raise FileNotFoundError(return_code, proc_stderr, remote_result_filename)
 
-        # Copy result file from backend to Covalent server
-        await self.run_async_subprocess(
-            [
-                "rsync",
-                "-e",
-                f"'ssh -i {self.ssh_key_file} -o StrictHostKeyChecking=no "
-                "-o UserKnownHostsFile=/dev/null -o LogLevel=ERROR'",
-                f"{self.username}@{self.address}:{remote_result_filename}",
-                f"{task_results_dir}/",
-            ],
-        )
+        proc = await conn.run(f"test -e {remote_result_filename}")
+        if proc.returncode != 0:
+            raise FileNotFoundError(proc.returncode, proc.stderr, remote_result_filename)
 
-        # Copy stdout, stderr from backend to Covalent server
-        await self.run_async_subprocess(
-            [
-                "rsync",
-                "-e",
-                f"'ssh -i {self.ssh_key_file} -o StrictHostKeyChecking=no "
-                "-o UserKnownHostsFile=/dev/null -o LogLevel=ERROR'",
-                f"{self.username}@{self.address}:{self.options['output']}",
-                f"{task_results_dir}/",
-            ],
-        )
-
-        await self.run_async_subprocess(
-            [
-                "rsync",
-                "-e",
-                f"'ssh -i {self.ssh_key_file} -o StrictHostKeyChecking=no "
-                "-o UserKnownHostsFile=/dev/null -o LogLevel=ERROR'",
-                f"{self.username}@{self.address}:{self.options['error']}",
-                f"{task_results_dir}/",
-            ],
-        )
-
+        # Copy result file from remote machine to Covalent server
         local_result_filename = os.path.join(task_results_dir, result_filename)
+        await asyncssh.scp((conn, remote_result_filename), local_result_filename)
+
+        # Copy stdout, stderr from remote machine to Covalent server
+        stdout_file = os.path.join(task_results_dir, os.path.basename(self.options["output"]))
+        stderr_file = os.path.join(task_results_dir, os.path.basename(self.options["error"]))
+
+        await asyncssh.scp((conn, self.options['output']), stdout_file)
+        await asyncssh.scp((conn, self.options['error']), stderr_file)
+
         async with aiofiles.open(local_result_filename, "rb") as f:
             contents = await f.read()
             result, exception = pickle.loads(contents)
         await async_os.remove(local_result_filename)
 
-        stdout_file = os.path.join(task_results_dir, os.path.basename(self.options["output"]))
         async with aiofiles.open(stdout_file, "r") as f:
             stdout = await f.read()
         await async_os.remove(stdout_file)
 
-        stderr_file = os.path.join(task_results_dir, os.path.basename(self.options["error"]))
         async with aiofiles.open(stderr_file, "r") as f:
             stderr = await f.read()
         await async_os.remove(stderr_file)
 
-        return result, stdout, stderr, exception
+        if exception:
+            raise RuntimeError(exception)
 
-    async def run(self, function, args, kwargs, task_metadata):
+        return result, stdout, stderr
+
+    async def run(self, function: Callable, args: List, kwargs: Dict, task_metadata: Dict):
 
         dispatch_id = task_metadata["dispatch_id"]
         node_id = task_metadata["node_id"]
@@ -354,7 +342,11 @@ wait
             )
 
         result = None
-        exception = None
+
+        ssh_success, conn = await self._client_connect()
+
+        if not ssh_success:
+            raise RuntimeError(f"Could not connect to host: '{self.address}' as user: '{self.username}'")
 
         async with aiofiles.tempfile.NamedTemporaryFile(
                 dir=self.cache_dir
@@ -367,39 +359,18 @@ wait
 
             # Create the remote directory
             app_log.debug(f"Creating remote work directory {self.remote_workdir} ...")
-            await self.run_async_subprocess(
-                [
-                    "ssh",
-                    "-i",
-                    self.ssh_key_file,
-                    "-o",
-                    "StrictHostKeyChecking=no",
-                    "-o",
-                    "UserKnownHostsFile=/dev/null",
-                    "-o",
-                    "LogLevel=ERROR",
-                    f"{self.username}@{self.address}",
-                    "mkdir",
-                    "-p",
-                    self.remote_workdir,
-                ],
-            )
+            cmd_mkdir_remote = f"mkdir -p {self.remote_workdir}"
+
+            mkdir_cache = await conn.run(cmd_mkdir_remote)
+            if client_err := mkdir_cache.stderr:
+                app_log.warning(client_err)
 
             # Copy the function to the remote filesystem
             func_filename = f"func-{dispatch_id}-{node_id}.pkl"
             remote_func_filename = os.path.join(self.remote_workdir, func_filename)
 
             app_log.debug(f"Copying function to remote fs: {remote_func_filename} ...")
-            await self.run_async_subprocess(
-                [
-                    "rsync",
-                    "-e",
-                    f"'ssh -i {self.ssh_key_file} -o StrictHostKeyChecking=no "
-                    "-o UserKnownHostsFile=/dev/null -o LogLevel=ERROR'",
-                    temp_f.name,
-                    f"{self.username}@{self.address}:{remote_func_filename}",
-                ],
-            )
+            await asyncssh.scp(temp_f.name, (conn, remote_func_filename))
 
             func_py_version = ".".join(function.args[0].python_version.split(".")[:2])
             app_log.debug(f"Python version: {func_py_version}")
@@ -418,59 +389,35 @@ wait
             # Copy the script to the remote filesystem
             remote_slurm_filename = os.path.join(self.remote_workdir, slurm_filename)
             app_log.debug(f"Copying slurm submit script to remote: {remote_slurm_filename} ...")
-            await self.run_async_subprocess(
-                [
-                    "rsync",
-                    "-e",
-                    f"'ssh -i {self.ssh_key_file} -o StrictHostKeyChecking=no "
-                    "-o UserKnownHostsFile=/dev/null -o LogLevel=ERROR'",
-                    temp_g.name,
-                    f"{self.username}@{self.address}:{remote_slurm_filename}",
-                ],
-            )
+            await asyncssh.scp(temp_g.name, (conn, remote_slurm_filename))
 
             # Execute the script
             remote_slurm_filename = os.path.join(self.remote_workdir, slurm_filename)
             app_log.debug(f"Executing the script: {remote_slurm_filename} ...")
-            return_code, proc_stdout, proc_stderr = await self.run_async_subprocess(
-                [
-                    "ssh",
-                    "-i",
-                    self.ssh_key_file,
-                    "-o",
-                    "StrictHostKeyChecking=no",
-                    "-o",
-                    "UserKnownHostsFile=/dev/null",
-                    "-o",
-                    "LogLevel=ERROR",
-                    f"{self.username}@{self.address}",
-                    "/bin/bash",
-                    "-l",
-                    "-c",
-                    f"'sbatch {remote_slurm_filename}'",
-                ],
-            )
-            if return_code == 0:
-                slurm_job_id = int(re.findall("[0-9]+", proc_stdout.decode("utf-8"))[0])
+
+            cmd_sbatch_run = f"sbatch {remote_slurm_filename}"
+            proc = await conn.run(cmd_sbatch_run)
+
+            if proc.returncode == 0:
+                slurm_job_id = int(re.findall("[0-9]+", proc.stdout))
             else:
-                raise RuntimeError(proc_stderr)
+                raise RuntimeError(proc.stderr)
 
             app_log.debug(f"Polling slurm with job_id: {slurm_job_id} ...")
-            await self._poll_slurm(slurm_job_id)
+            await self._poll_slurm(slurm_job_id, conn)
 
             app_log.debug(f"Querying result with job_id: {slurm_job_id} ...")
-            result, stdout, stderr, exception = await self._query_result(
-                result_filename, task_results_dir
+            result, stdout, stderr = await self._query_result(
+                result_filename, task_results_dir, conn
             )
 
-            if exception:
-                raise exception
 
             print(stdout)
             print(stderr, file=sys.stderr)
+            
+            app_log.debug("Closing SSH connection...")
+            conn.close()
+            await conn.wait_closed()
+            app_log.debug("SSH connection closed, execution finished returning result...")
 
-            app_log.debug(f"Execution finished...")
-
-            # FIX: covalent-mono's _run_task should parse exceptions from executor.execute()
-            # return result, stdout, stderr, exception
             return result
